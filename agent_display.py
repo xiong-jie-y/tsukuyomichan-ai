30de44c3a88a4b6d0b71715ab1d8c3488d093954
xiong-jie-y <fwvillage@gmail.com>
2021-11-04 21:58:12 +0900

    Add video generator.

--- agent_display.py
+++ agent_display.py
@@ -1,3 +1,4 @@
+import enum
 import emoji
 from espnet_model_zoo.downloader import ModelDownloader
 from tsukuyomichan_talksoft import TsukuyomichanTalksoft
@@ -106,14 +107,14 @@ class BodyController:
             import IPython; IPython.embed()
 
         pose[0, pose_parameters.get_parameter_index("neck_z")] = rate
-        if rate > 0.0:
-            pose[0, pose_parameters.get_parameter_index("eye_relaxed_left")] = 1.0
-            pose[0, pose_parameters.get_parameter_index("eye_relaxed_right")] = 1.0
-            pose[0, pose_parameters.get_parameter_index("eye_wink_left")] = 0.0
-            pose[0, pose_parameters.get_parameter_index("eye_wink_right")] = 0.0
-        else:
-            pose[0, pose_parameters.get_parameter_index("eye_relaxed_left")] = 0
-            pose[0, pose_parameters.get_parameter_index("eye_relaxed_right")] = 0
+        # if rate > 0.0:
+        #     pose[0, pose_parameters.get_parameter_index("eye_relaxed_left")] = 1.0
+        #     pose[0, pose_parameters.get_parameter_index("eye_relaxed_right")] = 1.0
+        #     pose[0, pose_parameters.get_parameter_index("eye_wink_left")] = 0.0
+        #     pose[0, pose_parameters.get_parameter_index("eye_wink_right")] = 0.0
+        # else:
+        #     pose[0, pose_parameters.get_parameter_index("eye_relaxed_left")] = 0
+        #     pose[0, pose_parameters.get_parameter_index("eye_relaxed_right")] = 0
 
 mouth_shapes = ["mouth_aaa", "mouth_iii", "mouth_uuu", "mouth_eee", "mouth_ooo", "mouth_delta"]
 
@@ -192,8 +193,55 @@ import onnxruntime
 from realesrgan import RealESRGANer
 from basicsr.archs.rrdbnet_arch import RRDBNet
 
+# class UpscaleMethod(enum.Enum):
+#     RealESRGAN = "realesrgan"
+#     RealESRGANOnnx = "realesrgan_onnx"
+
+class RealESRGANUpscaler:
+    def __init__(self):
+        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
+        self.upsampler = RealESRGANer(
+            scale=4,
+            model_path="RealESRGAN_x4plus_anime_6B.pth",
+            model=model,
+            tile=0,
+            tile_pad=10,
+            pre_pad=0,
+            half=False)
+
+    def upscale(self, pil_image):
+        output, _ = self.upsampler.enhance(np.array(pil_image), outscale=4)
+        pil_image = PIL.Image.fromarray(output, mode='RGB')
+
+        return pil_image
+
+class FastSRGANUpscaler:
+    def __init__(self):
+        self.super_resolution_session = onnxruntime.InferenceSession("generator2.onnx", providers = ['CUDAExecutionProvider'])
+
+    def upscale(self, pil_image):
+        s = time.time()
+        output_image_2 = self.super_resolution_session.run([], {"input_1": [np.array(pil_image)/255]})[0][0]
+        output_image_2 = (((output_image_2 + 1) / 2.) * 255).astype(np.uint8)
+        output_image_2 = cv2.resize(output_image_2, (output_image_2.shape[0] //2, output_image_2.shape[1]//2))
+        pil_image = PIL.Image.fromarray(output_image_2, mode='RGB')
+        # pil_image = pil_image.resize((pil_image.width // 2, pil_image.height //2 ))
+        print(time.time() - s)
+        return pil_image
+
+# class RealESRGANONNXUpscaler:
+#     def __init__(self):
+#         self.super_resolution_session = onnxruntime.InferenceSession("RealESRGAN_x4plus_anime_6B.onnx", providers = ['CUDAExecutionProvider'])
+
+#     def upscale(self, pil_image):
+#         # import IPython; IPython.embed()
+#         output_image_2 = self.super_resolution_session.run([], {"image.1": [np.transpose(np.array(pil_image), (2, 0, 1)) / 255]})[0][0]
+#         pil_image2 = PIL.Image.fromarray(numpy.uint8(numpy.rint(np.transpose(output_image_2[[2, 1, 0], :, :], (1, 2, 0)) * 255)), mode='RGB')
+#         return pil_image2
+
 class TsukuyomichanVisualizationGenerator:
-    def __init__(self, clock, upscale=True):
+    def __init__(self, clock, background_color=(0,0,0), upscale=True, 
+        upscale_method=RealESRGANUpscaler):
         self.device = torch.device("cuda:0")
 
         self.poser = tha2.poser.modes.mode_20.create_poser(self.device)
@@ -206,24 +254,15 @@ class TsukuyomichanVisualizationGenerator:
         self.mouth_shape_controller = MouthShapeController(clock, None)
         self.body_controller = BodyController(clock)
 
+        self.background_color = background_color
+
         self.clock = clock
 
         self.saying_something_ = False
         self.do_blink = True
         self.upscale = upscale
         if upscale:
-            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
-            self.upsampler = RealESRGANer(
-                scale=4,
-                model_path="RealESRGAN_x4plus_anime_6B.pth",
-                model=model,
-                tile=0,
-                tile_pad=10,
-                pre_pad=0,
-                half=False)
-
-            # self.super_resolution_session = onnxruntime.InferenceSession("generator3.onnx", providers = ['CUDAExecutionProvider'])
-            # self.super_resolution_session = onnxruntime.InferenceSession("RealESRGAN_x4plus_anime_6B.onnx", providers = ['CUDAExecutionProvider'])
+            self.upscaler = upscale_method()
 
         self.torch_input_image = extract_pytorch_image_from_PIL_image(extract_PIL_image_from_filelike("neautral_face.png")).to(self.device)
 
@@ -247,7 +286,7 @@ class TsukuyomichanVisualizationGenerator:
             self.pose[0, self.pose_parameters.get_parameter_index("eye_wink_right")] = blink_rate
             self.pose[0, self.pose_parameters.get_parameter_index("eye_wink_left")] = blink_rate
 
-        # self.body_controller.control(self.pose, self.pose_parameters)
+        self.body_controller.control(self.pose, self.pose_parameters)
         finished = self.mouth_shape_controller.control(self.pose, self.pose_parameters)
         if finished:
             self.torch_input_image = extract_pytorch_image_from_PIL_image(extract_PIL_image_from_filelike(f"neautral_face.png")).to(self.device)
@@ -272,26 +311,18 @@ class TsukuyomichanVisualizationGenerator:
         output_image = output_image.detach().cpu()
         numpy_image = numpy.uint8(numpy.rint(convert_output_image_from_torch_to_numpy(output_image) * 255.0))
         pil_image = PIL.Image.fromarray(numpy_image, mode='RGBA')
-        background = PIL.Image.new('RGBA', pil_image.size, (0,255,0))
+        background = PIL.Image.new('RGBA', pil_image.size, self.background_color)
         pil_image = PIL.Image.alpha_composite(background, pil_image).convert('RGB')
         # print(1 / (time.time() - s))
 
         if self.upscale:
             # pil_image = pil_image.resize((pil_image.width * 4, pil_image.height * 4), PIL.Image.ANTIALIAS)
             s = time.time()
-            # import IPython; IPython.embed()
-            # output_image_2 = self.super_resolution_session.run([], {"input.1": [np.array(pil_image).reshape(3, 256, 256) / 255]})[0][0]
-            # pil_image2 = PIL.Image.fromarray(numpy.uint8(numpy.rint(output_image_2.reshape(3, 1024*1024).transpose().reshape(1024, 1024, 3) * 255)), mode='RGB')
-            output, _ = self.upsampler.enhance(np.array(pil_image), outscale=4)
-            pil_image = PIL.Image.fromarray(output, mode='RGB')
+            pil_image = self.upscaler.upscale(pil_image)
+            print("Real ESRGan", 1 / (time.time() - s))
             # output_image_2 *= 255
             # output_image_2 = output_image_2[:,:,::-1]
 
-            # output_image_2 = self.super_resolution_session.run([], {"input_1": [np.array(pil_image)/255]})[0][0]
-            # output_image_2 = (((output_image_2 + 1) / 2.) * 255).astype(np.uint8)
-            # pil_image = PIL.Image.fromarray(numpy.uint8(numpy.rint(output_image_2.reshape(1024, 1024, 3))), mode='RGB')
-            # pil_image = pil_image.resize((pil_image.width // 2, pil_image.height //2 ))
-            # print("Real ESRGan", 1 / (time.time() - s))
             # pil_image.show()
             
 
@@ -308,12 +339,15 @@ class Speaker():
     def output(self, wav, fs):
         play_obj = sa.play_buffer(wav, 1, 2, fs)
 
+from english_to_kana import EnglishToKana
+
+
 class TsukuyomichanVisualizer:
     # load the example file included in the ESPnet repository
     MAX_WAV_VALUE = 32768.0
     fs = 24000
 
-    def __init__(self, talksoft, clock=WallClock(), wav_output=Speaker()):
+    def __init__(self, talksoft, clock=WallClock(), wav_output=Speaker(), background_color=None):
         d = ModelDownloader()
         aaa = d.download_and_unpack("kan-bayashi/csj_asr_train_asr_transformer_raw_char_sp_valid.acc.ave")
         # sentence = "今日の天気はとても良い。"
@@ -337,7 +371,25 @@ class TsukuyomichanVisualizer:
         self.clock = clock
         self.wav_output = wav_output
 
-        self.visualization_generator = TsukuyomichanVisualizationGenerator(self.clock)
+        self.visualization_generator = TsukuyomichanVisualizationGenerator(self.clock, background_color)
+
+        base_dictionary = {
+            "github": "ギットハブ",
+            "FastSRGAN": "ファストエスアールガン",
+            "GAN": "ガン",
+            "ESRGAN": "リアルイーエスアールガン",
+            "Real": "リアル",
+            "Bicubic": "バイキュービック",
+            "Realtime": "リアルタイム"
+        }
+
+        self.english_to_kana_dictionary = {}
+        # TODO: decide it is necessary.
+        for key in base_dictionary.keys():
+            self.english_to_kana_dictionary[key.lower()] = base_dictionary[key]
+            self.english_to_kana_dictionary[key] = base_dictionary[key]
+
+        self.english2kana = EnglishToKana()
 
     def saying_something(self):
         return self.visualization_generator.saying_something()
@@ -359,23 +411,26 @@ class TsukuyomichanVisualizer:
             emotion_label = self.emotion_label[emotion_index] if emotions[emotion_index] > 0.9 else None
             # import IPython; IPython.embed()
 
+            alphabet_replaced_sentence = ""
+            doc = self.nlp(sentence)
+            for sent in doc.sents:
+                for token in sent:
+                    if re.match(r"[a-zA-Z]+", token.orth_):
+                        yomi = ginza.reading_form(token)
+                        if re.match(r"[a-zA-Z]+", yomi) and yomi.lower() in self.english_to_kana_dictionary:
+                            yomi = self.english_to_kana_dictionary[yomi.lower()]
+                        another_yomi = self.english2kana.convert(yomi.lower())
+                        if re.match(r"[a-zA-Z]+", yomi) and another_yomi is not None:
+                            yomi = another_yomi
+                        # import IPython; IPython.embed()
+                        alphabet_replaced_sentence += yomi
+                    else:
+                        alphabet_replaced_sentence += token.orth_
+
+            print(alphabet_replaced_sentence)
             print(emotions)
 
-            s = time.time()
-            if len(sentence) > 200:
-                all_wavs = []
-                for partial_sentence in sentence.split("。"):
-                    wav = self.talksoft.generate_voice(partial_sentence, 1)
-                    wav = wav * self.MAX_WAV_VALUE
-                    wav = wav.astype(np.int16)
-                    all_wavs.append(wav)
-                wav = np.concatenate(wav)
-                print(time.time() - s)
-            else:
-                wav = self.talksoft.generate_voice(sentence, 1)
-                wav = wav * self.MAX_WAV_VALUE
-                wav = wav.astype(np.int16)
-                print(time.time() - s)
+            sentence = alphabet_replaced_sentence
 
             uttrs = []
             readings = []
@@ -388,6 +443,24 @@ class TsukuyomichanVisualizer:
                             uttrs.append(token.orth_)
                             readings.append(ginza.reading_form(token))
                             types.append(token.pos_)
+
+            sentence = "".join(uttrs)
+            s = time.time()
+            if len(alphabet_replaced_sentence) > 200:
+                all_wavs = []
+                for partial_sentence in alphabet_replaced_sentence.split("。"):
+                    wav = self.talksoft.generate_voice(partial_sentence, 1)
+                    wav = wav * self.MAX_WAV_VALUE
+                    wav = wav.astype(np.int16)
+                    all_wavs.append(wav)
+                wav = np.concatenate(wav)
+                print(time.time() - s)
+            else:
+                wav = self.talksoft.generate_voice(alphabet_replaced_sentence, 1)
+                wav = wav * self.MAX_WAV_VALUE
+                wav = wav.astype(np.int16)
+                print(time.time() - s)
+
 
             print(uttrs, readings, types)
             segments = None
--- generate_talking_video.py
+++ generate_talking_video.py
@@ -4,29 +4,56 @@ from tsukuyomichan_talksoft import TsukuyomichanTalksoft
 from agent_display import TsukuyomichanVisualizer
 
 
-manuscript = [
-    "テスト"
-    # "こんにちは、フリー素材キャラクターつくよみの非公式AIです。",
-    # 0.8,
-    # "私、最近、文章読み動画を作ることができるようになりました。",
-    # 0.8,
-    # "文章を用意していただければ、読み上げて動画にします。",
-    # 0.3,
-    # "読み上げ内容に合わせて、口を動かしたり、表情を変えたりもできます。",
-    # 0.5, 
-    # "例えば",
-    # 0.5, 
-    # "むかつく",
-    # 0.3, 
-    # "というように怒りの表現であれば、怒った表情になり",
-    # 0.3,
-    # "今日のカレーは美味しかった",
-    # 0.3,
-    # "というようにハッピーな表現であれば、ハッピーな表情になります。",
-    # 0.3,
-    # "この動画も文章を元に自動的に私が作りました。",
-    # 1.0
-]
+# manuscript = [
+#     "こんにちは、フリー素材キャラクターつくよみの非公式AIです。",
+#     0.8,
+#     "私、最近、文章読み動画を作ることができるようになりました。",
+#     0.8,
+#     "文章を用意していただければ、読み上げて動画にします。",
+#     0.3,
+#     "読み上げ内容に合わせて、口を動かしたり、表情を変えたりもできます。",
+#     0.5, 
+#     "例えば",
+#     0.5, 
+#     "むかつく",
+#     0.3, 
+#     "というように怒りの表現であれば、怒った表情になり",
+#     0.3,
+#     "今日のカレーは美味しかった",
+#     0.3,
+#     "というようにハッピーな表現であれば、ハッピーな表情になります。",
+#     0.3,
+#     "この動画も文章を元に自動的に私が作りました。",
+#     1.0
+# ]
+
+manuscript = """
+Talking Head Anime 2とは、一枚のキャラクターの顔画像のみを用いてキャラクターの口や目や眉毛を動かせる技術です。
+Githubでオープンソースで公開されています。
+"""
+
+manuscript = """
+これは、超解像モデルの中でもそこそこ動く、Real ESRGAN、FastSRGAN、Realtime Super ResolutionをBicubic補完を使った拡大結果です。
+実行速度はRTX 3070のPCで測定しました。
+アニメ画像の場合、Real ESRGANは非常にきれいに拡大してくれました。
+実行速度は10fps前後で、キャラクターのモーションをリアルタイムで表示するには厳しく、動画生成など、非リアルタイム処理に用いました。
+FastSRGANはBicubicよりましですが、若干ぼやけています。
+ただ、30fpsで動かすことができるため、512×512に縮小してリアルタイム処理に利用しました。
+Realtime Super Resolutionは実行速度が遅い割には、そんなにきれいではなかったため、今回は使いませんでした。
+1.5
+また、さらなる高速化を目指し、ONNX化とgraph optimizationも試してみました。
+ONNX化したモデルでは、消費ビデオメモリ量が数百から1GB増え、実行速度はあまり変わりませんでした。
+ORT化などさらなる最適化をかけた場合は実行速度が低下しました。
+これ以上の速度改善には今の所成功していません。
+"""
+
+import re
+
+manuscript = [s for s in manuscript.split("\n") if s.strip() != ""]
+for i in range(0, len(manuscript)):
+    if re.match(r"[0-9\.]+", manuscript[i]):
+        manuscript[i] = float(manuscript[i])
+
 import tempfile
 from moviepy.editor import AudioFileClip, CompositeAudioClip
 from moviepy.audio.AudioClip import AudioArrayClip
@@ -47,7 +74,14 @@ class VideoGenerator:
         self.wav = np.concatenate((self.wav, wav))
 
     def pad_with_empty(self):
-        self.wav = np.concatenate((self.wav, np.array([0] * int(self.fs * (self.time() - len(self.wav)/self.fs)))))
+        subtract_wavlen_videolen = int(self.fs * (self.time() - len(self.wav)/self.fs))
+        if subtract_wavlen_videolen > 0:
+            self.wav = np.concatenate((self.wav, np.array([0] * subtract_wavlen_videolen)))
+            return True
+        elif subtract_wavlen_videolen == 0:
+            return  True
+        else:
+            return False
 
     def add_video_frame(self, pil_image):
         self.pil_images.append(pil_image)
@@ -74,12 +108,13 @@ class VideoGenerator:
             
 video_generator = VideoGenerator()
 visualizer = TsukuyomichanVisualizer(
-    TsukuyomichanTalksoft(model_version='v.1.2.0'), clock=video_generator, wav_output=video_generator)
+    TsukuyomichanTalksoft(model_version='v.1.2.0'), clock=video_generator, wav_output=video_generator, background_color=(0,255,0))
 
 # import IPython; IPython.embed()
 for text in manuscript:
     if isinstance(text, str):
-        video_generator.pad_with_empty()
+        while not video_generator.pad_with_empty():
+            video_generator.add_video_frame(visualizer.visualize(None))
         video_generator.add_video_frame(visualizer.visualize(text))
 
         while visualizer.saying_something():
